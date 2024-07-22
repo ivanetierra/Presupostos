789bcde60a2e1358f8cb1f8574fb28f791316e03
ivan <ielizalde.dev@gmail.com>
2024-07-23 00:09:15 +0200

    move list of products to service

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -4,24 +4,3 @@ export interface Product {
   price: number;
   controlName: string;
 }
-
-export const products = [
-  {
-    title: 'Seo',
-    description: 'Programació duna web responsive completa',
-    price: 300,
-    controlName: 'seo',
-  },
-  {
-    title: 'Ads',
-    description: 'Programació duna web responsive completa',
-    price: 400,
-    controlName: 'ads',
-  },
-  {
-    title: 'Web',
-    description: 'Programació duna web responsive completa',
-    price: 500,
-    controlName: 'web',
-  },
-];
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Product, products } from '../models/product';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+
+   private products = [
+    {
+      title: 'Seo',
+      description: 'Programació duna web responsive completa',
+      price: 300,
+      controlName: 'seo',
+    },
+    {
+      title: 'Ads',
+      description: 'Programació duna web responsive completa',
+      price: 400,
+      controlName: 'ads',
+    },
+    {
+      title: 'Web',
+      description: 'Programació duna web responsive completa',
+      price: 500,
+      controlName: 'web',
+    },
+  ];
+
   constructor() {}
 
   getProducts(): Product[] {
-    return products;
+    return this.products;
   }
 }
